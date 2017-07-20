25591b4c386f1ca1db445590e8ccd2e47f9e0cd1
jptlightx005 <jptlightx005@gmail.com>
2017-07-20 19:47:09 +0800

    Added basic API to retrieve food menu

--- index.js
+++ index.js
@@ -13,14 +13,23 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+
 //Database
 var mysql = require('mysql');
 var con = mysql.createConnection(process.env.CLEARDB_DATABASE_URL, true);
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+app.get('/food/menu/get', function(request, response) {
+	con.connect(function(err) {
+		if (err) throw err;
+		console.log("Connected!");
+
+		con.query("SELECT * FROM food_menu", function (err, result, fields) {
+			if (err) throw err;
+			response.send(result);
+		});
+	});
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
